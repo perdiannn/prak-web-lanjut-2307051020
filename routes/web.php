a64110c817773c147a665ca5b2420eee5a380a63
Ferdian <ferdi4nn@gmail.com>
2025-05-25 13:11:37 +0700

    Menambahkan Storage File

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/', function () {
 
 // Route::get('/profile', [ProfileController::class, 'profile']);
 
+// Route::get('/profile/{nama}/{kelas}/{npm}', [UserController::class, 'profile'])->name('profile');
+
 Route::get('/user/profile', [UserController::class, 'profile']);
 
 Route::get('/profile/{nama}/{kelas}/{npm}', [UserController::class, 'profile']);
@@ -32,4 +34,22 @@ Route::get('/user/profile', action: [UserController::class, 'profile']);
 
 Route::get('/user/create', action: [UserController::class, 'create']);
 
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+
 Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+
+Route::get('/user', [UserController::class, 'index'])->name('user');
+
+Route::get('/user/list', [UserController::class, 'index'])->name('user.list');
+
+Route::get('/user/show{id}', [UserController::class, 'show'])->name('user.show');
+
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+
+Route::put('user/{id}', [UserController::class, 'show'])->name('user.show');
+
+Route::put('user/{id}', [UserController::class, 'update'])->name('user.update');
+
+Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+
+Route::delete('user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
